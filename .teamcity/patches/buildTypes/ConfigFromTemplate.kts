72f8623cd4b980119e1d79c676730d013077552e
TeamCity <TeamCity@TeamCity>
2019-06-28 12:52:13 +0300

    TeamCity change in 'Project with subprojects2' project: project settings were updated on disk

--- .teamcity/patches/buildTypes/ConfigFromTemplate.kts
+++ .teamcity/patches/buildTypes/ConfigFromTemplate.kts
@@ -15,27 +15,31 @@ changeBuildType(RelativeId("ConfigFromTemplate")) {
     steps {
         insert(0) {
             script {
-                name = "1"
-                scriptContent = "1"
+                name = "4"
+                scriptContent = "4"
             }
         }
         insert(1) {
-            script {
-                name = "2"
-                scriptContent = "2"
-            }
-        }
-        insert(2) {
             script {
                 name = "3 copy"
                 scriptContent = "3"
             }
         }
-        insert(3) {
+        insert(2) {
             script {
-                name = "4"
-                scriptContent = "4"
+                name = "2"
+                scriptContent = "2"
             }
         }
+        insert(3) {
+            script {
+                name = "1"
+                scriptContent = "1"
+            }
+        }
+        check(stepsOrder == arrayListOf<String>()) {
+            "Unexpected build steps order: $stepsOrder"
+        }
+        stepsOrder = arrayListOf("RUNNER_4", "RUNNER_3", "RUNNER_2", "RUNNER_1")
     }
 }
